80197046a3baa42a4ae4a886c2e6d843cdb46a77
Amadej Gradic <amadej.gradic@student.um.si>
2022-05-10 20:25:45 +0200

    Added toast onClick

--- app/src/main/java/com/example/smartboxpj/MainActivity.kt
+++ app/src/main/java/com/example/smartboxpj/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.smartboxpj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+    fun scanQrCode(view: View){
+        Toast.makeText(this, "Neuspešno skeniranje" ,Toast.LENGTH_SHORT).show()
+    }
+
 }
