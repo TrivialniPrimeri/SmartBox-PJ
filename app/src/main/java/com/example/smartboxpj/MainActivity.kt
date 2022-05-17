53abc5214fd0b3d3e6acc72fdba3c0577d39e401
Matic Šulc <matic.sulc1@student.um.si>
2022-05-18 00:16:39 +0200

    Testing out API implementation

--- app/src/main/java/com/example/smartboxpj/MainActivity.kt
+++ app/src/main/java/com/example/smartboxpj/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.smartboxpj
 
 
 import android.content.Intent
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 import android.view.View
 import android.widget.Toast
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.lang.Exception
+
+data class API_Response(val data: String, val result: String, val errorNumber: String){}
 
 open class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +26,29 @@ open class MainActivity : AppCompatActivity() {
 
     }
     fun scanQrCode(view: View){
+        val client = OkHttpClient()
+        val request: Request =  Request.Builder().url("https://ancient-savannah-30390.herokuapp.com/box/unlock/541").build()
+        try {
+            client.newCall(request).enqueue(object: Callback {
+                override fun onResponse(call: Call, response: Response) {
+                    val body = response?.body?.string()
+                    val gson = GsonBuilder().create()
+                    val resp = gson.fromJson(body, API_Response::class.java)
+                    val fos = FileOutputStream(File(filesDir, "test.mp3"))
+                    fos.write(Base64.decode(resp.data.toByteArray(), Base64.DEFAULT))
+                    fos.close()
+                    val player = MediaPlayer.create(applicationContext, Uri.parse("$filesDir/test.mp3"))
+                    player.start()
+
+                }
+
+                override fun onFailure(call: Call, e: IOException) {
+                    println(e.localizedMessage)
+                }
+            })
+        } catch (e: Exception){
+            println(e.message)
+        }
         Toast.makeText(this, "Neuspešno skeniranje" ,Toast.LENGTH_SHORT).show()
     }
 
