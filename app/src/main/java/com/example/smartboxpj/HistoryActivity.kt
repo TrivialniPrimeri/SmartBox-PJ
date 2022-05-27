e3033d11b879a45245d4e38acec9c0eb7c89889e
Matic Šulc <matic.sulc1@student.um.si>
2022-05-27 22:04:17 +0200

    Implemented unlocks history view

--- app/src/main/java/com/example/smartboxpj/HistoryActivity.kt
+++ app/src/main/java/com/example/smartboxpj/HistoryActivity.kt
@@ -3,6 +3,7 @@ package com.example.smartboxpj
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -11,13 +12,31 @@ import com.example.smartboxpj.databinding.ActivityHistoryBinding
 import com.franmontiel.persistentcookiejar.PersistentCookieJar
 import com.franmontiel.persistentcookiejar.cache.SetCookieCache
 import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor
+import com.google.gson.Gson
 import okhttp3.*
 import java.io.IOException
+import java.time.Instant
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+
+data class Box (
+    val nickname: String,
+    val boxId: String,
+)
+
+data class historyResponse(
+    val _id: String,
+    val success: Boolean,
+    val boxId: Box,
+    val createdAt: String
+)
 
 class HistoryActivity : MainActivity() {
 
-    private fun getDataFromAPI(ctx: Context) {
+    private fun getDataFromAPI(ctx: Context, data: MutableList<ItemsViewModel>, adapter: HistoryAdapter) {
+
         val cookieCache = SharedPrefsCookiePersistor(ctx).loadAll();
 
         val cookie = cookieCache.find { it.name == "refreshToken" }
@@ -30,40 +49,53 @@ class HistoryActivity : MainActivity() {
         try {
             client.newCall(request).enqueue(object: Callback {
                 override fun onResponse(call: Call, response: Response) {
-                    println(response.message)
                     if(response.code != 200){
                         runOnUiThread {
-                            Toast.makeText(applicationContext, "Napaka pri prijavi." , Toast.LENGTH_SHORT).show()
+                            Toast.makeText(applicationContext, "Napaka pri pridobivanju zgodovine." , Toast.LENGTH_SHORT).show()
                         }
                     }
                     else{
                         runOnUiThread {
-
+                            val obj = Gson().fromJson(response.body!!.string(), Array<historyResponse>::class.java)
+                            obj.forEach {
+                                data.add(
+                                    ItemsViewModel(
+                                    "${it.boxId.nickname} (${it.boxId.boxId})",
+                                    Instant.parse(it.createdAt).atZone(ZoneOffset.UTC).toLocalDateTime(),
+                                    it.success,
+                                    if (it.success) R.drawable.ic_boxicon3 else R.drawable.ic_boxicon3
+                                )
+                                )
+                                adapter.notifyItemInserted(data.size - 1)
+                                binding.progressBar.visibility = View.GONE
+                            }
                         }
                     }
                 }
                 override fun onFailure(call: Call, e: IOException) {
-                    println(e.localizedMessage)
+                    binding.progressBar.visibility = View.GONE
+                    runOnUiThread {
+                        Toast.makeText(applicationContext, "Napaka pri pridobivanju zgodovine." , Toast.LENGTH_SHORT).show()
+                    }
+
                 }
             })
         } catch (e: Exception){
-            println(e.message)
+            binding.progressBar.visibility = View.GONE
+            Toast.makeText(applicationContext, "Napaka pri pridobivanju zgodovine." , Toast.LENGTH_SHORT).show()
         }
     }
 
     private lateinit var binding: ActivityHistoryBinding
-    val data = mutableListOf<ItemsViewModel>()
+    var data = mutableListOf<ItemsViewModel>()
     val adapter = HistoryAdapter(data)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHistoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.progressBar.visibility = View.VISIBLE
+        getDataFromAPI(applicationContext, data, adapter)
 
-        getDataFromAPI(applicationContext)
-
-        for(i in 1..20){
-        data.add(ItemsViewModel("My title", LocalDateTime.now(), true, R.drawable.ic_boxicon3))
-        }
         val recycler = findViewById<RecyclerView>(R.id.history_recycler)
         recycler.layoutManager = LinearLayoutManager(this)
         recycler.adapter = adapter
--- app/src/main/java/com/example/smartboxpj/HistoryAdapter.kt
+++ app/src/main/java/com/example/smartboxpj/HistoryAdapter.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 data class ItemsViewModel(val title: String, val date: LocalDateTime,var success: Boolean, val image: Int) {
@@ -24,7 +25,8 @@ class HistoryAdapter(private val myList: List<ItemsViewModel>) : RecyclerView.Ad
     override fun onBindViewHolder(holder: HistoryAdapter.ViewHolder, position: Int) {
         val itemsViewModel = myList[position]
         holder.title.text = itemsViewModel.title
-        holder.date.text = itemsViewModel.date.toString()
+        val dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")
+        holder.date.text = itemsViewModel.date.format(dateTimeFormatter)
         holder.success.text = itemsViewModel.success.toString()
         holder.imageView.setImageResource(itemsViewModel.image)
     }
